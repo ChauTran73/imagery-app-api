7542c68360adcefbf4985578c358f5b1ce04240b
Chau Tran <cmtran7393@gmail.com>
2019-10-26 13:43:53 -0400

    seed

---
+++ seeds/seed.imagery_tables.sql
@@ -0,0 +1,156 @@
+BEGIN;
+
+TRUNCATE
+  imagery_comments,
+  imagery_images,
+  imagery_users
+  RESTART IDENTITY CASCADE;
+
+INSERT INTO imagery_users (email, full_name, password)
+VALUES
+  ('dunder.mifflin@yahoo.com', 'Dunder Mifflin',  'password'),
+  ('b.deboop@gmail.com', 'Bodeep Deboop',  'bo-password'),
+  ('c.bloggs@hotmail.com', 'Charlie Bloggs',  'charlie-password'),
+  ('s.smith@gmail.com', 'Sam Smith',  'sam-password'),
+  ('lexlor@yahoo.com', 'Alex Taylor', , 'lex-password'),
+  ('wippy123@outlook.com', 'Ping Won In', 'ping-password');
+
+INSERT INTO imagery_images (title, style, author_id, description, image_url)
+VALUES
+  ('First image!', 'Animal', 1,
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?',
+    'https://source.unsplash.com/mgaS4FlsYxQ'),
+
+  ('Second image!', 'Nature', 2,
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',
+    'https://source.unsplash.com/Ve8yy0Oi1rE'),
+  ('Third image!', 'People', 3,
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.',
+    'https://source.unsplash.com/UNGw3QD3DIA'),
+  ('Fourth image', 'Travel', 4,
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, consequuntur. Cum quo ea vero, fugiat dolor labore harum aut reprehenderit totam dolores hic quaerat, est, quia similique! Aspernatur, quis nihil?',
+    'https://source.unsplash.com/WvvDPMBD8Eg'),
+  ('Fifth image', 'Outdoor', 5,
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet soluta fugiat itaque recusandae rerum sed nobis. Excepturi voluptas nisi, labore officia, nobis repellat rem ab tempora, laboriosam odio reiciendis placeat?',
+    'https://source.unsplash.com/NhlKx6Uvm3E'),
+  ('Sixth image', 'Art', 6,
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'https://source.unsplash.com/jGAWr8wPeh'),
+  ('Seventh image', 'Love', 1,
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, voluptatum nam culpa minus dolore ex nisi recusandae autem ipsa assumenda doloribus itaque? Quos enim itaque error fuga quaerat nesciunt ut?',
+    'https://source.unsplash.com/OdvNB15f7Uo'),
+  ('Eigth image', 'Design', 2,
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur sequi sint beatae obcaecati voluptas veniam amet adipisci perferendis quo illum, dignissimos aspernatur ratione iusto, culpa quam neque impedit atque doloribus!',
+    'https://source.unsplash.com/sV5JQ1ICrrg'),
+  ('Ninth image', 'Story', 3,
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos architecto repellat, in amet soluta exercitationem perferendis eius perspiciatis praesentium voluptate nisi deleniti eaque? Rerum ea quisquam dolore, non error earum?',
+    'https://source.unsplash.com/Kr_RijTa0kg')
+  
+
+INSERT INTO imagery_comments (
+  text,
+  image_id,
+  user_id
+) VALUES
+  (
+    'This image is amazing',
+    1,
+    2
+  ),
+  (
+    'Yeh I agree it''s amazing',
+    1,
+    3
+  ),
+  (
+    'I would go so far as to say it''s double amazing',
+    1,
+    4
+  ),
+  (
+    'A-mazing!',
+    1,
+    5
+  ),
+  (
+    'That''s some interesting lorems you raise',
+    2,
+    6
+  ),
+  (
+    'Yeh totally I''d never thought about lorems like that before',
+    2,
+    1
+  ),
+  (
+    'So you''re saying consectetur adipisicing elit?',
+    2,
+    3
+  ),
+  (
+    'Sixth? You mean sith?!!',
+    4,
+    6
+  ),
+  (
+    'What do you call an evil procrastinator? Darth Later! Hahahahaha!',
+    4,
+    4
+  ),
+  (
+    'Ten ten ten ten ten ten ten!',
+    10,
+    3
+  ),
+  (
+    'Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam!!!',
+    10,
+    5
+  ),
+  (
+    '5, 6, 7, 8! My boot-scootin'' baby is drivin'' me crazy...!',
+    7,
+    1
+  ),
+  (
+    'My obsession from a western! My dance floor date',
+    7,
+    2
+  ),
+  (
+    'My rodeo Romeo. A cowboy god from head to toe',
+    7,
+    3
+  ),
+  (
+    'Wanna make you mine. Better get in line. 5, 6, 7, 8!',
+    7,
+    4
+  ),
+  (
+    'Just a lonely comment',
+    9,
+    6
+  ),
+  (
+    'Really? Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris??!',
+    6,
+    5
+  ),
+  (
+    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris for sure!!',
+    6,
+    1
+  ),
+  (
+    'WOAH!!!!!',
+    8,
+    2
+  ),
+  (
+    '°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸',
+    8,
+    4
+  );
+
+COMMIT;
