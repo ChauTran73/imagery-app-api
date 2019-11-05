1b6e8cacbd29cb0d6e3c4bedcf582a1ddc98fa6d
Chau Tran <cmtran7393@gmail.com>
2019-11-04 23:04:21 -0500

    hash password

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,7 +1,7 @@
 //create user service objects to manage db interactions
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 const xss = require('xss')
-
+const bcrypt = require('bcryptjs')
 
 const UsersService = {
     getAllUsers(knex) {
@@ -43,6 +43,9 @@ const UsersService = {
             return 'Password must contain 1 upper case, lower case, number and special character'
         }
         return null
+    },
+    hashPassword(password) {
+        return bcrypt.hash(password, 12)
     },
     serializeUser(user) {
         return {
